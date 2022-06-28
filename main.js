d847734b2748f684ca3dee0eb6d5a5889e5a140f
KANYEcode <107776400+KANYEcode@users.noreply.github.com>
2022-06-28 00:55:22 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
-const copyToClipboard = str => navigator.clipboard.writeText(`ǀ${str}ǀ`);
+// load and process tags
+let labels = {};
+let csv = await(await fetch('tags.csv')).text()
+csv.split('\r\n').slice(1).forEach(row => {
+  let items = row.split(',');
+  let code = items[1];
+  let tags = items.slice(3).filter(i => i.length);
+  labels[code] = tags.join(' ');
+});
+
+
+const copyToClipboard = str => {
+  navigator.clipboard.writeText(`ǀ${str}ǀ`);
+  M.toast({ text: `Copied ${str}` });
+};
 const container = document.getElementById('container');
 
 for (let i = 90; i < 3000; i++) {
@@ -9,9 +23,13 @@ for (let i = 90; i < 3000; i++) {
     const img = new Image();
     img.src = url;
     img.title = `${code}`;
+    img.setAttribute('tags', labels[code])
     img.addEventListener('load', () => {
       img.addEventListener('click', () => copyToClipboard(img.title));
       container.appendChild(img);
     });
   }, 5 * i);
 }
+
+const searchBox = document.getElementById('searchBox');
+searchBox.addEventListener('input', e => container.childNodes.forEach(i => i.classList.toggle('hidden', !i.getAttribute('tags').includes(searchBox.value.trim()))));
