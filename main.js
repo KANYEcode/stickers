c58a321721b419e8c29cabac6b9ff5790a67ff7c
KANYEcode <107776400+KANYEcode@users.noreply.github.com>
2022-06-28 19:24:16 -0700

    Update main.js

--- main.js
+++ main.js
@@ -3,28 +3,41 @@ const copyToClipboard = str => {
   M.toast({ text: `Copied ${str}` });
 };
 
-const sanitize = str => str.trim().toLowerCase().replaceAll(/[.,\/#!$%\^&\*;:{}=\-_`~()'"]/g, '');
-
+const sanitize = str => str.trim().replaceAll(/[.,\/#!$%\^&\*;:{}=\-_`~()'"]/g, '').toLowerCase();
 
 // load and process tags
 let labels = {};
-let csv = await(await fetch('tags.csv')).text()
+let csv = await(await fetch('tags.csv')).text();
+
+let uniques = {};
+
 csv.split(/\r?\n/).slice(1).forEach(row => {
   let items = row.split(',');
+  if (items[0].length) {
+    uniques[items[0]] = null;
+  }
   let code = items[1];
   let tags = items.slice(3).filter(i => i.length);
   labels[code] = sanitize(tags.join(' '));
+});
+
+const searchBox = document.getElementById('searchBox');
+searchBox.addEventListener('input', e => container.childNodes.forEach(i => i.classList.toggle('hidden', !i.getAttribute('tags').includes(sanitize(searchBox.value)))));
+const instances = M.Autocomplete.init(searchBox, {
+  data: uniques,
+  limit: 1,
 });
 
 
 const container = document.getElementById('container');
 
 for (let i = 90; i < 5000; i++) {
-  const code = i.toString(36)
+  const code = i.toString(36);
   const url = `https://prd.foxtrotstream.xyz/a/stk/${code}.webp`;
 
   const img = new Image();
   img.classList.add('hidden');
+
   img.src = url;
   img.title = `${code}`;
   img.setAttribute('tags', labels[code]);
@@ -37,6 +50,3 @@ for (let i = 90; i < 5000; i++) {
     img.addEventListener('click', () => copyToClipboard(img.title));
   });
 }
-
-const searchBox = document.getElementById('searchBox');
-searchBox.addEventListener('input', e => container.childNodes.forEach(i => i.classList.toggle('hidden', !i.getAttribute('tags').includes(sanitize(searchBox.value)))));
